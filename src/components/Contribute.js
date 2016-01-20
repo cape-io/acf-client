4db6904b5d4d58b9f0348921f03e1413e0c3109c
Jared VanderHook <jared.vanderhook@gmail.com>
2016-01-20 11:36:04 -0600

    Created Contribute.js form

---
+++ src/components/Contribute.js
@@ -0,0 +1,43 @@
+Import React, { PropTypes } from 'react'
+
+// Online contribution donation form
+function Contribute({ }) {
+    return (
+
+      <form>
+        <div className="radio">
+          <label>
+            <input type="radio" name="optionsRadios" id="optionsRadios1" defaultValue="option1" defaultChecked />
+            One-Time Contribution
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input type="radio" name="optionsRadios" id="optionsRadios2" defaultValue="option2" />
+            Monthly Sustaining Contribution
+          </label>
+        </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputName">Recognition Name(s)</label>
+            <input type="text" className="form-control" id="exampleInputName" placeholder="Name" />
+              <span id="helpBlock" className="help-block">Please list your name(s) as you would like to be acknowledged in ACF's donor list.</span>
+          </div>
+          <div className="form-group">
+            <textarea className="form-control" rows={3} defaultValue={""} />f
+          </div>
+          <button type="submit" className="btn btn-default">Continue
+          </button>
+      </form>
+    );
+  }
+});
+
+Loading.propTypes = {
+  Contribute: PropTypes.checked.isRequired,
+  Sustaining:
+}
+Loading.defaultProps = {
+
+}
+
+export default Contribute
